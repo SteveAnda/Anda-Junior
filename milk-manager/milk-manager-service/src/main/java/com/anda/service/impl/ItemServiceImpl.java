50e74875190bd57363b33fcada67e82ea157c017
wanganda <wanganda@topwisesz.com>
2018-05-04 21:01:38 +0800

    日志能够正常输出,包括mybatis sql语句, 还有添加了工程运行的sql文件

--- milk-manager/milk-manager-service/src/main/java/com/anda/service/impl/ItemServiceImpl.java
+++ milk-manager/milk-manager-service/src/main/java/com/anda/service/impl/ItemServiceImpl.java
@@ -3,16 +3,21 @@ package com.anda.service.impl;
 import com.anda.mapper.TbItemMapper;
 import com.anda.pojo.TbItem;
 import com.anda.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class ItemServiceImpl implements ItemService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ItemServiceImpl.class);
 
     @Autowired
     private TbItemMapper itemMapper;
 
     @Override
     public TbItem getItemById(long itemId) {
-        System.out.println("-----===============================-------------ItemServiceImpl------------------getItemById方法执行-----------------------");
+
+        LOGGER.info("======================ItemServiceImpl=====================getItemById方法执行");
         return itemMapper.selectByPrimaryKey(itemId);
     }
 }
